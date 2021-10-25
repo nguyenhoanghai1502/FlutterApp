ee95a935252bbd614da2b1a3eb7460539313a8db
hunghuy201280 <humghuy201280@gmail.com>
2021-10-25 14:21:27 +0700

    [Update] - Update example

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_mental_health/view_models/app_provider.dart';
+import 'package:flutter_mental_health/views/home_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 class MentalHealthApp extends StatelessWidget {
   const MentalHealthApp({Key? key}) : super(key: key);
@@ -10,14 +15,20 @@ class MentalHealthApp extends StatelessWidget {
       //Put Figma size here
       designSize: const Size(1080, 1920),
       builder: () {
-        return const MaterialApp(
+        return MaterialApp(
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'),
+            Locale('vi'),
+          ],
+          locale: context.watch<AppProvider>().locale,
           debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            backgroundColor: Colors.lightBlue,
-            body: Center(
-              child: Text("Hello World!"),
-            ),
-          ),
+          home: const HomeScreen(),
         );
       },
     );
---
+++ lib/configs/app_config.dart
@@ -0,0 +1,4 @@
+class AppConfigs {
+  static const kServerUri = "SERVER_URI";
+  static const apiUrl = "url.com";
+}
